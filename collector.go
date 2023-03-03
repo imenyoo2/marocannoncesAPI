ca23752fc42155ffb02025e4008521e560c23ff1
ayoub <ayoubaitelaouad@gmail.com>
2023-03-03 19:47:02 +0100

    refactoring the project: adding a new collector file

---
+++ collector.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "strings"
+	"github.com/gocolly/colly"
+  "time"
+  "encoding/json"
+)
+
+
+func (app *application) marocAnnonesCollect() {
+
+  c := colly.NewCollector()
+  // setting callback functions
+  c.OnRequest(func(r *colly.Request) {
+    fmt.Println("Visiting", r.URL)
+  })
+  c.OnError(func(_ *colly.Response, err error) {
+    log.Println("Something went wrong:", err)
+  })
+
+  c.OnHTML("article.listing > a:nth-child(1)", func(e *colly.HTMLElement) {
+    e.Request.Visit(e.Attr("href"))
+  })
+
+  // matching the title
+  c.OnHTML("#content > div.used-cars > div.description.desccatemploi > h1", func(e *colly.HTMLElement) {
+    //fmt.Println("getting title")
+    title := strings.ReplaceAll(strings.ReplaceAll(e.Text, "\n", ""), "  ", "")
+    e.Request.Ctx.Put("title", title)
+    //fmt.Println("title added to context")
+    app.data[title] = map[string]interface{}{"title": title}
+    //fmt.Println("title added to data")
+  })
+
+  // matching Annonceur
+  c.OnHTML(".infoannonce > dl:nth-child(1) > dd:nth-child(2)", func(e *colly.HTMLElement) {
+    // adding annonceur feild to data
+    app.data[e.Request.Ctx.Get("title")]["Annonceur"] = e.Text
+  })
+
+  c.SetRequestTimeout(1 * time.Minute)
+
+  c.Visit("https://www.marocannonces.com/categorie/309/Emploi/Offres-emploi.html")
+
+  // TODO: change this from printing to serveing via http
+  jsonStr, err := json.Marshal(app.data)
+  check(err)
+  fmt.Println(string(jsonStr))
+}
--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
-  "time"
-  "strings"
-  "encoding/json"
-  //"os"
 
-	"github.com/gocolly/colly"
 )
 
 func check(err error) {
@@ -17,45 +11,19 @@ func check(err error) {
   }
 }
 
+type application struct {
+  data      map[string]map[string]interface{}
+}
 
 func main() {
   // initializing a new collecotor
   data := map[string]map[string]interface{}{}
-  c := colly.NewCollector()
-  // setting callback functions
-  c.OnRequest(func(r *colly.Request) {
-    fmt.Println("Visiting", r.URL)
-  })
-  c.OnError(func(_ *colly.Response, err error) {
-    log.Println("Something went wrong:", err)
-  })
-  c.OnResponse(func(r *colly.Response){
-    //fmt.Println("Visited", r.Request.URL)
-  })
 
-  c.OnHTML("article.listing > a:nth-child(1)", func(e *colly.HTMLElement) {
-    e.Request.Visit(e.Attr("href"))
-  })
+  app := &application {
+    data: data,
+  }
+  app.marocAnnonesCollect()
 
-  // matching the title
-  c.OnHTML("#content > div.used-cars > div.description.desccatemploi > h1", func(e *colly.HTMLElement) {
-    //fmt.Println("getting title")
-    title := strings.ReplaceAll(strings.ReplaceAll(e.Text, "\n", ""), "  ", "")
-    e.Request.Ctx.Put("title", title)
-    //fmt.Println("title added to context")
-    data[title] = map[string]interface{}{"title": title}
-    //fmt.Println("title added to data")
-  })
-  // matching Annonceur
-  c.OnHTML(".infoannonce > dl:nth-child(1) > dd:nth-child(2)", func(e *colly.HTMLElement) {
-    //fmt.Println("getting annonceur")
-    data[e.Request.Ctx.Get("title")]["Annonceur"] = e.Text
-    //fmt.Println("annonceur added to data")
-  })
-  c.SetRequestTimeout(1 * time.Minute)
-  c.Visit("https://www.marocannonces.com/categorie/309/Emploi/Offres-emploi.html")
-  jsonStr, err := json.Marshal(data)
-  check(err)
-  fmt.Println(string(jsonStr))
+
 
 }
